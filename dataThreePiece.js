6994b47903ca15aad8dc43980f4d4e7c818f2a98
BlindOracle <55452191+BlindOracle@users.noreply.github.com>
2024-12-02 14:12:07 +0100

    update and export messages

---
+++ dataThreePiece.js
@@ -0,0 +1,23 @@
+
+export const messages = {
+    firstMessage: [
+    'dreary', 'stressful', 'frustrating', 'exhausting', 'boring', 'gloomy'
+    ],
+    secondMessage: [
+    'there\’s still room for something good.',
+    'you can find moments of strength.',
+    'a small step forward still counts.',
+    'it’s okay to pause and breathe.',
+    'your efforts matter more than you think.',
+    'the sun will rise again tomorrow.'
+    ],
+    thirdMessage: [
+    "you are doing great!",
+    "keep pushing forward!",
+    "success is on the horizon.",
+    "believe in yourself!",
+    "tomorrow is a new beginning.",
+    "great things are coming your way!"]
+}
+
+// initial testing array 
